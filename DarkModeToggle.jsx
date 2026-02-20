4e6425738546915cee4b6011231d0eaa8b5053d4
wa1eed21 <wa1eed21.w1992@gmail.com>
2026-02-20 04:17:28 +0300

    Delete DarkModeToggle.jsx

--- DarkModeToggle.jsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import { useEffect, useState } from "react";
-export default function DarkModeToggle() {
-  const [dark,setDark]=useState(false);
-  useEffect(()=>{if(dark)document.documentElement.classList.add("dark");else document.documentElement.classList.remove("dark");},[dark]);
-  return (
-    <button onClick={()=>setDark(!dark)} className="bg-gray-700 px-2 py-1 rounded hover:bg-gray-600">{dark?"☀️":"🌙"}</button>
-  );
-}
